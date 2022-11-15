4e9248526450273bef507354095e5bf343f5c0d4
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-11-15 10:37:04 -0700

    Refactored the Render Method to better handle rendering

--- worlds/World.java
+++ worlds/World.java
@@ -22,8 +22,13 @@ public class World {
 	}
 	
 	public void render(Graphics g) {
-		for(int y = 0;y < height; y++) {
-			for(int x = 0; x < width;x++) {
+		int xStart = (int) Math.max(0, game.getGameCamera().getxOffset() / Tile.TILEWIDTH);
+		int xEnd = (int) Math.min(width, (game.getGameCamera().getxOffset() + game.getWidth()) / Tile.TILEWIDTH + 1);
+		int yStart = (int) Math.max(0, game.getGameCamera().getyOffset() / Tile.TILEHIEGHT);
+		int yEnd = (int) Math.min(height, (game.getGameCamera().getyOffset() + game.getHeight()) / Tile.TILEHIEGHT + 1);
+		
+		for(int y = yStart;y < yEnd; y++) {
+			for(int x = xStart; x < xEnd;x++) {
 				getTile(x, y).render(g, (int) (x * Tile.TILEWIDTH - game.getGameCamera().getxOffset()),(int) (y * Tile.TILEHIEGHT - game.getGameCamera().getyOffset()));
 				
 			}

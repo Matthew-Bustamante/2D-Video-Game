88a35891718d5ee9b6d212b9d0c9e17ff40dea25
CrusaderCoder73 <mbus7337@gmail.com>
2022-11-23 14:47:44 -0700

    Fixed Texture Issue

--- display/graphics/Assets.java
+++ display/graphics/Assets.java
@@ -19,7 +19,6 @@ public class Assets {
 	public static void init() {
 		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/spriteV2.png"));
 		SpriteSheet playerSheet = new SpriteSheet(ImageLoader.loadImage("/textures/playerSpriteSheet.png"));
-		
 		//Player Animation BufferedImage Object Arrays
 		player_down = new BufferedImage[2];
 		player_up = new BufferedImage[2];
@@ -34,11 +33,10 @@ public class Assets {
 		player_up[0] = playerSheet.crop(width * 2, 0, width, height);
 		player_up[1] = playerSheet.crop(width * 3, 0, width, height);
 		
-		//Player right Animation Crops
+		//Player right Animation 
 		player_right[0] = playerSheet.crop(0, 32, width, height);
 		player_right[1] = playerSheet.crop(32, 32, width, height);
 		
-		//Player left Animation Crops
 		player_left[0] = playerSheet.crop(32 * 2, 32, width, height);
 		player_left[1] = playerSheet.crop(32 * 3, 32, width, height);
 		

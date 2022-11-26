251402fb7235e83799c9ce36cdbcc7baf846b72d
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-11-25 19:20:07 -0700

    Switched file reader with input stream

--- states/GameState.java
+++ states/GameState.java
@@ -16,7 +16,7 @@ public class GameState extends State{
 	
 	public GameState(Handler handler) {
 		super(handler);
-		world = new World(handler, "res/worlds/world1.txt");
+		world = new World(handler, "worlds/world1.txt");
 		handler.setWorld(world);
 		player = new Player(handler, 100, 100);
 		

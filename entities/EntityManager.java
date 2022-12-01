3fc451d9471a3ac82957a53c56d5df6734dd5802
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-12-01 13:13:02 -0700

    Added better rendering between player and entities

--- entities/EntityManager.java
+++ entities/EntityManager.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import entities.creatures.Player;
 import game.Handler;
@@ -11,6 +12,15 @@ public class EntityManager {
 	private Handler handler;
 	private Player player;
 	private ArrayList<Entity> entities;
+	private Comparator<Entity> renderSorter = new Comparator<Entity>() {
+		@Override
+		public int compare(Entity a, Entity b) {
+			if(a.getY() + a.getHeight() < b.getY() + b.getHeight()) {
+				return -1;
+			}
+			return 1;
+		}
+	};
 	
 	public EntityManager(Handler handler, Player player) {
 		this.handler = handler;
@@ -24,6 +34,7 @@ public class EntityManager {
 			Entity e = entities.get(i);
 			e.tick();
 		}
+		entities.sort(renderSorter);
 		
 	}
 	

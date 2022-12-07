ef5fdc9ebd1a579f866c7ad3cc374a3c66ff66ae
CrusaderCoder73 <mbustam9@msudenver.edu>
2022-12-07 12:37:43 -0700

    Created UnitTest for Assets

--- src/test/java/TestAssets.java
+++ src/test/java/TestAssets.java
@@ -2,13 +2,25 @@ package src.test.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.awt.image.BufferedImage;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import display.graphics.Assets;
 class TestAssets {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAssetWorldTextures() {
+		Assets.init();
+		BufferedImage testWaterAsset = Assets.water;
+		BufferedImage testDirtAsset = Assets.dirt;
+		BufferedImage testGrassAsset = Assets.grass;
+		BufferedImage testWallAsset = Assets.wall;
+		assertNotNull(testWaterAsset);
+		assertNotNull(testDirtAsset);
+		assertNotNull(testGrassAsset);
+		assertNotNull(testWallAsset);
+		
 	}
 
 }
